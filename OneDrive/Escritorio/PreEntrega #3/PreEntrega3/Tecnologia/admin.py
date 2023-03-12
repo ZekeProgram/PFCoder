21f99968a2ca413beb041bd50606b6bbabcdcd1f
ZekeProgram <123603963+ZekeProgram@users.noreply.github.com>
2023-03-12 00:33:37 -0300

    Delete OneDrive/Escritorio/PreEntrega #3/PreEntrega3 directory

--- OneDrive/Escritorio/PreEntrega #3/PreEntrega3/Tecnologia/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Computadores, Televisores, Telefonos
-
-# Register your models here.
-
-admin.site.register(Computadores)
-admin.site.register(Televisores)
-admin.site.register(Telefonos)
--- OneDrive/Escritorio/PreEntrega #3/PreEntrega3/Tecnologia/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TecnologiaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Tecnologia'
